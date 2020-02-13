6d68479cdde7756925869c53026b9aee2e6761e0
iBowie <31174978+iBowie@users.noreply.github.com>
2020-02-13 17:08:24 +0500

    added provider extensions

---
+++ Rocket.Unturned/Extensions/ProviderExtension.cs
@@ -0,0 +1,34 @@
+﻿using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+
+namespace Rocket.Unturned.Extensions
+{
+    public static class ProviderExtension
+    {
+        public static IEnumerable<UnturnedPlayer> GetUnturnedPlayers()
+        {
+            foreach (var sp in Provider.clients)
+            {
+                if (sp == null || sp.player == null)
+                    continue;
+                UnturnedPlayer up = UnturnedPlayer.FromSteamPlayer(sp);
+                yield return up;
+            }
+        }
+        public static IEnumerable<UnturnedPlayer> GetUnturnedPlayers(Func<UnturnedPlayer, bool> filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException("filter");
+            foreach (var sp in Provider.clients)
+            {
+                if (sp == null || sp.player == null)
+                    continue;
+                UnturnedPlayer up = UnturnedPlayer.FromSteamPlayer(sp);
+                if (filter.Invoke(up))
+                    yield return up;
+            }
+        }
+    }
+}
